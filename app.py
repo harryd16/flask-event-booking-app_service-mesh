eb0b51675991479c9a35f604228023a3f6e5db01
Harrison Doran <harrydoran16@gmail.com>
2019-05-22 01:27:11 +1000

    Add failed login user feedback

--- app.py
+++ app.py
@@ -75,19 +75,13 @@ def new_event():
 # login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    print('test message')
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-        print('user %s logged in', current_user)
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        print(user)
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-
-            print('password %s for userid %s incorrect', form.password.data, form.username.data)
-
+            flash('Invalid username or password', 'error')
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
